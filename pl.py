c6a2260291ee2606820c26a488b7d1da559ee372
Promisery <jiangtt17@gmail.com>
2021-03-14 15:10:38 +0800

    training

--- pl.py
+++ pl.py
@@ -12,11 +12,7 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from models.resnet import ResNet18
 import argparse
 
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--pe', default=0, type=int)
-args = parser.parse_args()
 
-pl.seed_everything(990121)
 
 class Model(pl.LightningModule):
     def __init__(self, pe=0):
@@ -54,6 +50,11 @@ class Model(pl.LightningModule):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+    parser.add_argument('--pe', default=0, type=int)
+    args = parser.parse_args()
+
+    pl.seed_everything(990121)
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
         transforms.RandomHorizontalFlip(),
@@ -69,12 +70,12 @@ if __name__ == '__main__':
     trainset = torchvision.datasets.CIFAR10(
         root='./data', train=True, download=True, transform=transform_train)
     trainloader = torch.utils.data.DataLoader(
-        trainset, batch_size=128, shuffle=True, num_workers=4)
+        trainset, batch_size=128, shuffle=True, num_workers=2, pin_memory=True)
 
     testset = torchvision.datasets.CIFAR10(
         root='./data', train=False, download=True, transform=transform_test)
     testloader = torch.utils.data.DataLoader(
-        testset, batch_size=100, shuffle=False, num_workers=4)
+        testset, batch_size=100, shuffle=False, num_workers=0, pin_memory=True)
 
     pe = args.pe
     print(pe)
